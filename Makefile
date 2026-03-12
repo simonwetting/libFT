ddd7705cd9a5dd1c20d454338a04ecf7ebcfd037
Simon Wetting <simonwetting@hotmail.com>
2026-03-12 14:09:11 +0100

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,19 @@
+NAME = libft.a
+FILENAMES = ft_strncmp ft_isprint ft_strrchr ft_memmove ft_isalnum ft_isalpha ft_strlcat ft_memcpy ft_strlcpy ft_toupper ft_memchr ft_strnstr ft_isdigit ft_strlen ft_tolower ft_strdup ft_substr ft_memcmp ft_atoi ft_calloc ft_strchr ft_memset ft_bzero
+
+OBJECTS = $(FILENAMES:%=%.o)
+FILES = $(FILENAMES:%=%.c)
+
+all: $(NAME) $(OBJECTS)
+
+$(NAME): $(OBJECTS)
+	cc -c $(FILES) -Wall -Werror -Wextra
+	ar rc $(NAME) $(OBJECTS)
+
+clean:
+	rm -f $(OBJECTS)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
--- libft.h
+++ libft.h
@@ -6,13 +6,37 @@
 /*   By: swetting <swetting@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/03/10 16:49:31 by swetting      #+#    #+#                 */
-/*   Updated: 2026/03/10 17:09:18 by swetting      ########   odam.nl         */
+/*   Updated: 2026/03/11 18:10:34 by swetting      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
 
-#include <stddef.h>
-#include <stdio.h>
+int		ft_atoi(const char *nptr);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+size_t	ft_strlcpy(char *dest, char *src, unsigned int size);
+size_t	ft_strlen(const char *s);
+int		ft_strncmp(const char *s1, const char *s2, unsigned int n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_strrchr(const char *s, int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 #endif
